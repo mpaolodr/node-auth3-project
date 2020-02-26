2f238cdd38f9deeaf9a82e226484b5e0351bf688
mpaolodr <mpaolodr_1504@yahoo.com>
2020-02-26 16:58:16 -0500

    created restricted middleware

---
+++ api/middlewares/restrictedMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const { jwtSecret } = require("../config/secrets.js");
+
+function restricted(req, res, next) {
+  const token = req.header.authorization;
+
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ errorMessage: "Unauthorized" });
+      } else {
+        req.user = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ errorMessage: "Invalid token" });
+  }
+}
+
+module.exports = restricted;
